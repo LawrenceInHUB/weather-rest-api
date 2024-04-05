9aa2af3743d3e4c73019c81e811a2fe656e432a2
Raveen Perera <raveenperera@icloud.com>
2024-04-06 00:23:09 +0530

    minor change.

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/', districtRoutes);
+app.use('/api/', districtRoutes);
 
 const PORT = process.env.PORT || 8010;
 app.listen(PORT, () => {
