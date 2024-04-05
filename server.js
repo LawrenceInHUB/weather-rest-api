087115bd51d9c3b7e0a2b503c23e415be733033f
Raveen Perera <raveenperera@icloud.com>
2024-04-06 00:13:17 +0530

    minor changes

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv")
+require("dotenv").config();
 const districtRoutes = require('./routes/districtRoutes');
-const cors = require('cors');
 
 const app = express();
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
 
-app.use('/api/', districtRoutes);
+app.use('/', districtRoutes);
 
 const PORT = process.env.PORT || 8010;
 app.listen(PORT, () => {
